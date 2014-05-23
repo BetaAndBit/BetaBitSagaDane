58d9a44568222033d4ebece35e5bf0e17ff08839
pbiecek <przemyslaw.biecek@gmail.com>
2014-05-23 10:13:30 +0200

    fix

--- R/cegielki.R
+++ R/cegielki.R
@@ -6,7 +6,7 @@ cegielki <- function(dane, tytul =  deparse(substitute(dane)), osx = c(19,35), o
 }
 
 mniejszywa <- function(dane, tytul =  deparse(substitute(dane)), osx = c(19,35), osy=c(0,4)) {
-  plot(ecdf(dane, las=1, ylab=tytul, xlab="", main="", xlim=osx,  bty="n", lwd=3, yaxt="n", xaxt="n")
+  plot(ecdf(dane), las=1, ylab=tytul, xlab="", main="", xlim=osx,  bty="n", lwd=3, yaxt="n", xaxt="n")
   axis(1, osx[1]:osx[2], col="white", col.ticks="black")
   axis(2, seq(0,1,0.2), paste0(seq(0,100,20),"%"), col="white", col.ticks="white", las=1)
   abline(h=seq(0.2,0.8,0.2), lty=2, col="grey")
